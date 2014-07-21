1bcab65d6f616834b2e6ee02ce599bddc3c5db5d
Scott Sperling <ssperlin@yahoo-inc.com>
2014-07-20 18:57:52 -0700

    add package definitions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 dota-lineup-analyzer
 ====================
+
+http://dev.dota2.com/showthread.php?t=47115
--- driver.py
+++ driver.py
@@ -2,6 +2,6 @@ from api import API
 
 api = API()
 
-print api.get_matches(600, 677739681)
+# print api.get_matches(600, 677739681)
 
 
