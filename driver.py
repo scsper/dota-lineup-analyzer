08ead7f25ddaef758288216a2118218317dc46ed
Scott Sperling <scsper@gmail.com>
2014-07-21 20:38:43 -0700

    changing driver.py to run the serializer

--- driver.py
+++ driver.py
@@ -1,13 +1,15 @@
 from api import API
 from models import Heroes
 from models import Lineup
+from models import League
+from cache import LeagueSaver
+
 import os
 import requests
 
 api = API()
-heroes = Heroes()
-lineup = Lineup()
-# print api.get_matches(600, 677739681)
+league = League("the_international_2014")
+LeagueSaver().save(league)
+# print lineup.lineupInMatch(matches[1])
 
-matches = heroes.hero_matches(29)
-print lineup.lineupInMatch(matches[1])
+
