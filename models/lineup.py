7878d6d039bf4b20ef52bc27dd2d632f09260236
Scott Sperling <scsper@gmail.com>
2014-07-26 15:05:42 -0700

    make pretty printing a little prettier

--- models/lineup.py
+++ models/lineup.py
@@ -13,7 +13,7 @@ class Lineup:
 
     def __str__(self):
         return "[" + str(self.heroes[0]) + ", " + str(self.heroes[1]) + ", " + \
-        str(self.heroes[2]) + ", " + str(self.heroes[3]) + ", " + str(self.heroes[4]) + " ]"
+        str(self.heroes[2]) + ", " + str(self.heroes[3]) + ", " + str(self.heroes[4]) + "]"
 
     def to_JSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
