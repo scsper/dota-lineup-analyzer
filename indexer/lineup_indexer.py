21ec5e1a3f744cc6869f6d7e0633c0cfb64367b4
Scott Sperling <scsper@gmail.com>
2014-07-26 15:04:28 -0700

    fix minor data access errors

--- indexer/lineup_indexer.py
+++ indexer/lineup_indexer.py
@@ -3,23 +3,21 @@ class LineupIndexer:
         self.heroesToLineups = {}
 
     def index(self, league):
-        print league
-        print league.matches
         for match in league.matches:
             self._add_lineup_to_index(match.radiant)
             self._add_lineup_to_index(match.dire)
 
 
     def _add_lineup_to_index(self, lineup):
-        for hero in lineup:
-            name = hero['name']
+        for hero in lineup.heroes:
+            name = hero.name
 
-            if name not in heroesToLineups:
-                heroesToLineups[name] = []
+            if name not in self.heroesToLineups:
+                self.heroesToLineups[name] = []
 
-            heroesToLineups[name].append(lineup)
+            self.heroesToLineups[name].append(lineup)
 
 
     def find_hero(self, hero_name):
-        for lineup in heroesToLineups[hero_name]:
+        for lineup in self.heroesToLineups[hero_name]:
             print lineup
