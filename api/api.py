779c90ea824963e38f7983c53934cfd2669669ac
Scott Sperling <scsper@gmail.com>
2014-07-27 23:47:05 -0700

    add api for player stats call

--- api/api.py
+++ api/api.py
@@ -6,6 +6,7 @@ BASE_URL = "https://api.steampowered.com/IDOTA2Match_570"
 
 MATCH_HISTORY_URL = BASE_URL + "/GetMatchHistory/V001/?key=" + API_KEY
 MATCH_DETAILS_URL = BASE_URL + "/GetMatchDetails/V001/?key=" + API_KEY
+PLAYER_STATS_URL =  BASE_URL + '/GetTournamentPlayerStats/v1/?key=' + API_KEY
 
 class API:
     def get_matches(self, league_id, starting_match_id=None, matches_requested=None):
@@ -30,5 +31,10 @@ class API:
         url = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v001/?key=" + API_KEY + "&language=en_us"
 
         return requests.get(url).json()["result"]["heroes"]
+
+    def get_player_stats(self, league_id, account_id):
+        url = PLAYER_STATS_URL + '&league_id=' + str(league_id) + '&account_id=' + str(account_id)
+
+        return requests.get(url).json()['result']
 
 
