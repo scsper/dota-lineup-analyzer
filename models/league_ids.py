5052c6dce64b9f66ff41101c4034de31f4879832
Scott Sperling <scsper@gmail.com>
2014-07-27 23:47:50 -0700

    make a distinction between the qualifiers and lan portion of the international because they have to be handled differently

--- models/league_ids.py
+++ models/league_ids.py
@@ -1,7 +1,8 @@
 class LeagueIds:
     def __init__(self):
         self.ids = {
-            "the_international_2014": 600
+            "the_international_2014": 600,
+            "the_international_2014_qualifiers": 600
         }
 
     def getLeagueId(self, league):
