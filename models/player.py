87d1cc586c5db71b5e66f611ce037a7a681b1456
Scott Sperling <scsper@gmail.com>
2014-08-09 16:35:30 -0700

    be able to save a player

--- models/player.py
+++ models/player.py
@@ -1,8 +1,12 @@
 from services import LeagueService
+import json
 
 class Player:
     def __init__(self, id, name):
         self.id = id
+        self.name = name
         self.matches = LeagueService(id, name, 'player').retrieve_matches()
-        print matches
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
