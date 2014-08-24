06744c5c89ce49f81456f45fac7bf5e97b53c010
Scott Sperling <scsper@gmail.com>
2014-08-24 01:16:28 -0700

    add search bar

--- client/components/Body.jsx
+++ client/components/Body.jsx
@@ -4,6 +4,8 @@ var Body = React.createClass({
     render: function() {
         return (
             <div className="container">
+                <Search />
+
                 <div className="starter-template">
                     <h1>Bootstrap starter template</h1>
                     <p className="lead">Use this document as a way to quickly start any new project.<br></br> All you get is this text and a mostly barebones HTML document.</p>
