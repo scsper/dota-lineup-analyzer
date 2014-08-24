3f655257b0e7b879a7666200fa6dde7b4e02980f
Scott Sperling <scsper@gmail.com>
2014-08-23 17:54:29 -0700

    add basic bootstrap styling

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Navigation />
+                <Body />
+            </div>
+        );
+    }
+});
---
+++ client/components/Navigation.jsx
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+
+var Navigation = React.createClass({
+    render: function() {
+        return (
+            <div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Project name</a>
+                    </div>
+                    <div className="collapse navbar-collapse">
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
