e4a71fd89b90afb08098339e3cdca597bf75a5ec
Scott Sperling <scsper@gmail.com>
2014-08-24 19:12:03 -0700

    clean up starter code

--- client/components/Body.jsx
+++ client/components/Body.jsx
@@ -5,11 +5,6 @@ var Body = React.createClass({
         return (
             <div className="container">
                 <Search />
-
-                <div className="starter-template">
-                    <h1>Bootstrap starter template</h1>
-                    <p className="lead">Use this document as a way to quickly start any new project.<br></br> All you get is this text and a mostly barebones HTML document.</p>
-                </div>
             </div>
         );
     }
