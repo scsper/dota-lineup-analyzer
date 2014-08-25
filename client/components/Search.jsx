d79a54bfd0fa4881d97e488214ce7081e35d5821
Scott Sperling <scsper@gmail.com>
2014-08-24 19:12:20 -0700

    touch up css

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -3,7 +3,7 @@
 var Search = React.createClass({
     render: function() {
         return (
-            <form role="form">
+            <form className="search" role="form">
                 <input type="text" className="form-control" placeholder="Enter heroes"></input>
             </form>
         );
