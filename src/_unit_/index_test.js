6c0fb5655f4ac929a8a233b6586f2eaacb31ac1c
Scott Sperling <scsper@gmail.com>
2015-05-13 20:37:04 -0700

    comment out dummy test

--- src/_unit_/index_test.js
+++ src/_unit_/index_test.js
@@ -2,7 +2,7 @@ var index = require('../index.js'),
     helper = require('../../test/setup/unit');
 
 describe('./index.js', function() {
-    it('equals 1', function() {
+    xit('equals 1', function() {
         expect(index()).to.equal(1);
     });
 });
