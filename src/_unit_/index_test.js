f96820e75b6b799ded7e1aeee6058e9847292188
Scott Sperling <scsper@gmail.com>
2015-05-13 20:38:51 -0700

    fix test

--- src/_unit_/index_test.js
+++ src/_unit_/index_test.js
@@ -1,6 +1,3 @@
-var index = require('../index.js'),
-    helper = require('../../test/setup/unit');
-
 describe('./index.js', function() {
     xit('equals 1', function() {
         expect(index()).to.equal(1);
