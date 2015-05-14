1fe0668eaf0d9b9c8119eab71999ebf8776b1a1e
Scott Sperling <scsper@gmail.com>
2015-05-13 21:29:27 -0700

    try again to deploy to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node ./src/run.js
+web: node index.js
--- src/run.js
+++
@@ -1,6 +0,0 @@
-var app = require('./server/app.js'),
-    DEFAULT_PORT = 3000;
-
-app.listen(DEFAULT_PORT, function() {
-    console.log('listening for connection on port ' + DEFAULT_PORT);
-});
