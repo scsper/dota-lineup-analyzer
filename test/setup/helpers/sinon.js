f1f6e5995f057c8e3acb2f508094a8afa6f68cab
Scott Sperling <scsper@gmail.com>
2015-05-26 23:22:12 -0700

    Add mockery.

--- test/setup/helpers/sinon.js
+++ test/setup/helpers/sinon.js
@@ -11,5 +11,4 @@ module.exports = function() {
     afterEach(function() {
         this.sandbox.restore();
     });
-
 };
--- test/setup/unit.js
+++ test/setup/unit.js
@@ -1,5 +1,7 @@
 var chaiHelper = require('./helpers/chai'),
-    sinonHelper = require('./helpers/sinon');
+    sinonHelper = require('./helpers/sinon'),
+    mockeryHelper = require('./helpers/mockery');
 
 chaiHelper();
 sinonHelper();
+mockeryHelper();
