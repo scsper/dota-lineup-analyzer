688b01e0215f421c97620bf1527a0c2c6b29d7a0
Scott Sperling <scsper@gmail.com>
2015-05-26 23:28:25 -0700

    Move fixture to separate file.

---
+++ src/server/middleware/_unit_/cache_fixture.js
@@ -0,0 +1,43 @@
+module.exports = {
+    id: 600,
+    matches: [{
+        dire: {
+            heroes: [{
+                "id": 42,
+                "name": "Wraith King"
+            }, {
+                "id": 17,
+                "name": "Storm Spirit"
+            }, {
+                "id": 57,
+                "name": "Omniknight"
+            }, {
+                "id": 33,
+                "name": "Enigma"
+            }, {
+                "id": 49,
+                "name": "Dragon Knight"
+            }]
+        },
+        id: 772548096,
+        radiant: {
+            heroes: [{
+                "id": 65,
+                "name": "Batrider"
+            }, {
+                "id": 101,
+                "name": "Skywrath Mage"
+            }, {
+                "id": 81,
+                "name": "Chaos Knight"
+            }, {
+                "id": 91,
+                "name": "Io"
+            }, {
+                "id": 80,
+                "name": "Lone Druid"
+            }]
+        },
+        winner: "dire"
+    }]
+};
--- src/server/middleware/_unit_/get_from_cache_test.js
+++ src/server/middleware/_unit_/get_from_cache_test.js
@@ -4,49 +4,7 @@ var getFromCache = require('../get_from_cache.js');
 describe('./get_from_cache', function() {
     it('gets the tournament from the cache', function() {
         // mockery uses paths from the caller, not the test.
-        mockery.registerMock('../cache/the_international_2014.json', {
-            id: 600,
-            matches: [{
-                dire: {
-                    heroes: [{
-                        "id": 42,
-                        "name": "Wraith King"
-                    }, {
-                        "id": 17,
-                        "name": "Storm Spirit"
-                    }, {
-                        "id": 57,
-                        "name": "Omniknight"
-                    }, {
-                        "id": 33,
-                        "name": "Enigma"
-                    }, {
-                        "id": 49,
-                        "name": "Dragon Knight"
-                    }]
-                },
-                id: 772548096,
-                radiant: {
-                    heroes: [{
-                        "id": 65,
-                        "name": "Batrider"
-                    }, {
-                        "id": 101,
-                        "name": "Skywrath Mage"
-                    }, {
-                        "id": 81,
-                        "name": "Chaos Knight"
-                    }, {
-                        "id": 91,
-                        "name": "Io"
-                    }, {
-                        "id": 80,
-                        "name": "Lone Druid"
-                    }]
-                },
-                winner: "dire"
-            }]
-        });
+        mockery.registerMock('../cache/the_international_2014.json', require('./cache_fixture.js'));
 
         var tournament = getFromCache('the_international_2014');
 
