9be07215043e77fd58c31bcaea09f4c0f40d064d
Scott Sperling <scsper@gmail.com>
2015-05-28 15:15:55 -0700

    Remove unnecessary test.

--- src/_unit_/index_test.js
+++
@@ -1,8 +0,0 @@
-// require('../../test/setup/unit.js');
-
-describe('./index.js', function() {
-    it('equals 1', function() {
-        this.sandbox.stub();
-        expect(1).to.equal(1);
-    });
-});
