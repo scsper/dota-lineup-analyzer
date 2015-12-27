08c31af900a164d91711ddd17ae86424fa33e576
Jessica Sperling <jessicaysperling@gmail.com>
2015-12-27 00:25:17 -0800

    Add pick and ban order

--- src/browser/components/hero.jsx
+++ src/browser/components/hero.jsx
@@ -9,13 +9,20 @@ const Hero = React.createClass({
        hero: React.PropTypes.object.isRequired
     },
 
+    renderOrder(order) {
+        if(order !== undefined){
+            return <div className={'seq'}>{`${order + 1}`}</div> ;
+        }
+        return null;
+    },
+
     render() {
         const {hero} = this.props;
         const hasPickOrBanClass = classNames({ 'isBan' :  hero.is_pick === false}, {'isPick' : hero.is_pick === true});
-
         return (
             <li className={'hero'}>
                 <img className={hasPickOrBanClass} src={ImageUrls[hero.hero_id]} />
+                {this.renderOrder(hero.order)}
             </li>
         );
     }
